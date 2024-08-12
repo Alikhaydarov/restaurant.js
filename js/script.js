f09af4ece787f9a7c82cf23aad45c4dbb961cbf0
Ali <alikhaydarov01@gmail.com>
2024-08-12 17:40:16 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+	//tabs
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabContents = document.querySelectorAll('.tab_content'),
 		tabParents = document.querySelector('.tabheader__items')
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 
-	function ShowTabContent(index = 0){
+	function ShowTabContent(index = 0) {
 		tabContents[index].classList.add('show', 'fade')
 		tabContents[index].classList.remove('hide')
 		tabs[index].classList.add('tabheader__item_active')
@@ -23,18 +23,118 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	HideTabContents()
 	ShowTabContent()
-	
-	tabParents.addEventListener('click' , event => {
-		const target = event.target
-		
 
-		if(target && target.classList.contains('tabheader__item')){
-			tabs.forEach((tab,index)=>{
-				if(target === tab){
+	tabParents.addEventListener('click', event => {
+		const target = event.target
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((tab, index) => {
+				if (target === tab) {
 					HideTabContents()
 					ShowTabContent(index)
 				}
 			})
 		}
 	})
+	//Loader
+	const loaderWrapper = document.querySelector('.loader-wrapper')
+
+	setTimeout(() => {
+		loaderWrapper.style.display = 'none'
+	}, 1500)
+	// Timer
+
+	const deadline = '2024-08-31'
+
+	function getTimeRemaining(endtime) {
+		let days, hours, minutes, seconds
+		const time = Date.parse(endtime) - Date.parse(new Date())
+
+		if (time <= 0) {
+			days = 0
+			hours = 0
+			minutes = 0
+			seconds = 0
+		} else {
+			;(days = Math.floor(time / (1000 * 60 * 60 * 24))),
+				(hours = Math.floor((time / (1000 * 60 * 60)) % 24)),
+				(minutes = Math.floor((time / (1000 * 60)) % 60)),
+				(seconds = Math.floor((time / 1000) % 60))
+		}
+
+		return {
+			totalTime: time,
+			days,
+			hours,
+			minutes,
+			seconds,
+		}
+	}
+	function formatNumber(number) {
+		if (number >= 0 && number < 10) {
+			return `0${number}`
+		} else {
+			return number
+		}
+	}
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000)
+
+		updateClock()
+
+		function updateClock() {
+			const time = getTimeRemaining(endtime)
+
+			days.textContent = formatNumber(time.days)
+			hours.textContent = formatNumber(time.hours)
+			minutes.textContent = formatNumber(time.minutes)
+			seconds.textContent = formatNumber(time.seconds)
+
+			if (time.totalTime <= 0) {
+				clearInterval(timeInterval)
+			}
+		}
+	}
+
+	setClock('.timer', deadline)
+
+	//modal
+
+	const modal = document.querySelector('.modal'),
+		modalOpenBtns = document.querySelectorAll('[data-modal]'),
+		modalCloseBtn = document.querySelector('[data-modal-close]')
+
+	const openModal = () => {
+		modal.classList.add('show')
+		modal.classList.remove('hide')
+		document.body.style.overflow = 'hidden'
+		clearInterval(modal)
+	}
+	const closeModal = () => {
+		modal.classList.add('hide')
+		modal.classList.remove('show')
+		document.body.style.overflow = 'auto'
+	}
+	modalOpenBtns.forEach(btn => {
+		btn.addEventListener('click', openModal)
+	})
+	modalCloseBtn.addEventListener('click', closeModal)
+
+	modal.addEventListener('click', event => {
+		if (event.target === modal) {
+			closeModal()
+		}
+	})
+	document.addEventListener('keydown', event => {
+		if (event.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal()
+		}
+	})
+	const modalTimerId = setTimeout(openModal, 6000)
 })
